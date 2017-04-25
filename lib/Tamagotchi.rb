1b592b038cbdc32aff83939e2bba587a84888778
Asia Kane and Jason Ainsworth <jason@>
2017-04-25 14:47:18 -0700

    determine if tamagotchi is alive based on food level and health level

--- lib/Tamagotchi.rb
+++ lib/Tamagotchi.rb
@@ -1,18 +1,20 @@
 class Tamagotchi
   define_method(:initialize) do |name|
     @name = name
-    @health = 10
-    @food_level = 10
-    @sleep_level = 10
-    @activity_level = 0
+    @health_level = 10 #effected by food, sleep, activity
+    @food_level = 10 #effected by feeding, activity, sleep, time
+    @sleep_level = 10 #effected by sleep, time
+    @activity_level = 0 #effected by sleep, walk, run, surf, xfit
   end
 
+###############################
+#Define instance variables
   define_method(:name) do
     @name
   end
 
-  define_method(:health) do
-    @health
+  define_method(:health_level) do
+    @health_level
   end
 
   define_method(:food_level) do
@@ -27,11 +29,15 @@ class Tamagotchi
     @activity_level
   end
 
-  define_method(:is_alive?) do
-    @food_level > 0
+###############################
+#Levels are being updated
+
+  define_method(:update_time) do
+    @food_level -= 1
+    @sleep_level -= 1
   end
 
-  define_method(:set_food_level) do
+  define_method(:update_food_level) do
     if @activity_level < 3
       @food_level -= 2
     elsif @activity_level < 5
@@ -41,34 +47,67 @@ class Tamagotchi
     else
       @food_level -= 10
     end
+    self.update_health_level()
   end
 
+  define_method(:update_health_level) do
+    if @food_level < 5
+       @health_level -= 2
+    end
+    if @sleep_level < 2
+      @health_level -= 3
+    end
+    if @activity_level < 3
+      @health_level -= 1
+    end
+  end
+###############################
+#Tamagotchi is doing things
+#Eat
+  #Need feed method
+  define_method(:feed) do
+    @food_level += 3
+    self.update_time()
+  end
+
+#Sleep
+define_method(:sleep) do
+  @activity_level = 0
+  @food_level -= 4
+  @sleep_level = 11
+  self.update_time()
+  self.update_health_level()
+end
+
+#Activities
   define_method(:walk) do
     @activity_level += 2
+    self.update_food_level()
+    self.update_time()
   end
 
   define_method(:run) do
     @activity_level += 4
+    self.update_food_level()
+    self.update_time()
   end
 
   define_method(:surf) do
     @activity_level += 7
+    self.update_food_level()
+    self.update_time()
   end
 
   define_method(:crossfit) do
     @activity_level += 10
+    self.update_food_level()
+    self.update_time()
   end
 
-  define_method(:sleep) do
-    @activity_level = 0
-    @food_level -= 5
-  end
+###############################
 
-  define_method(:update_health) do
-    if @food_level < 5
-      @health -= 2
-    end
+  define_method(:is_alive?) do
+    @food_level > 0 && @health_level > 0
   end
-
 
 end
--- spec/Tamagotchi_spec.rb
+++ spec/Tamagotchi_spec.rb
@@ -7,7 +7,7 @@ describe(Tamagotchi)do
     it('will create a new Tamagotchi and set the name and life level') do
       my_pet = Tamagotchi.new('Rex')
       expect(my_pet.name()).to(eq('Rex'))
-      expect(my_pet.health()).to(eq(10))
+      expect(my_pet.health_level()).to(eq(10))
       expect(my_pet.food_level()).to(eq(10))
       expect(my_pet.sleep_level()).to(eq(10))
       expect(my_pet.activity_level()).to(eq(0))
@@ -24,7 +24,6 @@ describe(Tamagotchi)do
       my_pet = Tamagotchi.new('Rex')
       my_pet.run()
       my_pet.crossfit()
-      my_pet.set_food_level()
       expect(my_pet.is_alive?()).to(eq(false))
     end
   end
@@ -44,14 +43,34 @@ describe(Tamagotchi)do
     end
   end
 
-  describe('#update_health') do
+  describe('#update_health_level') do
     it('subtracts 2 from the health if food level drops below 5') do
       my_pet = Tamagotchi.new('Rex')
       my_pet.crossfit()
-      my_pet.update_health()
-      expect(my_pet.health()).to(eq(8))
+      expect(my_pet.health_level()).to(eq(8))
     end
   end
 
+  describe('#feed') do
+    it('adds 3 to the food_level') do
+      my_pet = Tamagotchi.new('Rex')
+      my_pet.feed()
+      expect(my_pet.food_level()).to(eq(12))
+    end
+  end
+
+  describe('#update_time') do
+    it('decreases food level by 1') do
+      my_pet = Tamagotchi.new('Rex')
+      my_pet.update_time()
+      expect(my_pet.food_level()).to(eq(9))
+    end
+
+    it('decreases sleep level by 1') do
+      my_pet = Tamagotchi.new('Rex')
+      my_pet.update_time()
+      expect(my_pet.sleep_level()).to(eq(9))
+    end
+  end
 
 end
